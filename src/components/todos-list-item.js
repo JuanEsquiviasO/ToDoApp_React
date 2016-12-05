147cb46e0a20656cd533082e839503b49ecd75d1
Juan Esquivias <juanesquiviasotazu@gmail.com>
2016-12-05 15:52:00 -0500

    todosl-list and add styles and pointer modified

--- src/components/todos-list-item.js
+++ src/components/todos-list-item.js
@@ -9,6 +9,23 @@ export default class TodosListItem extends React.Component {
 		}
 	}
 
+	renderTaskSection() {
+		const {	task, isCompleted } = this.props;
+
+		const taskStyle = {
+			color: isCompleted ? 'green' : 'red',
+			cursor: 'pointer'
+		};
+
+		return (
+			<td style={taskStyle}
+				onClick={this.props.toggleTask.bind(this, task)}
+			>
+				{task}
+			</td>
+		);
+	}
+
 	renderActionsSection() {
 		if (this.state.isEditing) {
 			return (
@@ -30,7 +47,7 @@ export default class TodosListItem extends React.Component {
 	render() {
 		return (
 			<tr>
-				<td>{this.props.task}</td>
+				{this.renderTaskSection ()}
 				{this.renderActionsSection()}
 			</tr>
 		);
@@ -45,47 +62,5 @@ export default class TodosListItem extends React.Component {
 	}
 }
 
-// import React from 'react';
 
-// export default class TodosListItem extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-
-// 		this.state = {
-// 			isEditing: false
-// 		};
-// 	}
-
-// 	renderActionsSection() {
-// 		if (this.state.isEditing) {
-// 			return (
-// 				<td>
-// 					<button>Save</button>
-// 					<button>Cancel</button>
-// 				</td>
-// 			);
-// 		}
-
-// 		return (
-// 			<td>
-// 				<button onClick={this.onEditClick.bind(this)}>Edit</button>
-// 				<button>Delete</button>
-// 			</td>
-// 		);
-// 	}
-
-// 	render() {
-// 		return (
-// 			<tr>
-// 				<td>{this.props.task}</td>
-// 				{this.renderActionsSection.bind()}
-// 			</tr>
-// 		);
-// 	}
-
-// 	onEditClick() {
-// 		this.setState({	isEditing: true	});
-// 	}
-// }
-
-// // me quede en 26:42 video
+// me quede en 36:06 video
